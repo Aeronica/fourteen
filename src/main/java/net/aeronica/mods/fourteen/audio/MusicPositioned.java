65193f8d715c43666b74cd2d0a2294e2f34df1e1
Aeronica <pmboese@gmail.com>
2019-07-19 12:26:18 -0500

    Experiments in volume cutoff at distance.

--- src/main/java/net/aeronica/mods/fourteen/audio/MusicPositioned.java
+++ src/main/java/net/aeronica/mods/fourteen/audio/MusicPositioned.java
@@ -34,7 +34,7 @@ public class MusicPositioned extends MxSound
             Vec3d vec3d = new Vec3d(mc.player.posX, mc.player.posY, mc.player.posZ);
             BlockPos blockPos = audioData.getBlockPos();
             float distance = (float) vec3d.distanceTo(new Vec3d(blockPos.getX(), blockPos.getY(), blockPos.getZ()));
-            this.volume = (float) MathHelper.lerp(MathHelper.clamp((4 / (distance + .001)), 0.0F, 1F), 0.0, 4);
+            this.volume = (float) MathHelper.clamp(MathHelper.lerp(MathHelper.clamp((4 / (distance + .001)), 0.0F, 1F), -1, 4), 0, 4);
             LOGGER.debug("PosSound {}, dist {}, volume {}", audioData.getBlockPos(), distance, volume);
         }
     }
