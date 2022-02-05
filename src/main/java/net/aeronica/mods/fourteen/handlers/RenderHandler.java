aa07dd277cf33a9a37d3ed24dcb39d8ac39028cb
Aeronica <pmboese@gmail.com>
2022-02-05 14:21:33 -0600

    Mixin to the ParticleManager renderParticle method instead for our stage area rendering.

--- src/main/java/net/aeronica/mods/fourteen/handlers/RenderHandler.java
+++ src/main/java/net/aeronica/mods/fourteen/handlers/RenderHandler.java
@@ -1,14 +1,15 @@
 package net.aeronica.mods.fourteen.handlers;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.aeronica.mods.fourteen.caps.stages.ServerStageAreaProvider;
 import net.aeronica.mods.fourteen.render.StageAreaRenderer;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.settings.GraphicsFanciness;
+import net.minecraft.client.renderer.culling.ClippingHelper;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
@@ -22,23 +23,19 @@ public class RenderHandler
 
     private RenderHandler() { /* NOP */ }
 
-    public static void renderLast(MatrixStack pMatrixStack, IRenderTypeBuffer.Impl pBuffer, double pCamX, double pCamY, double pCamZ)
+    public static void renderLast(MatrixStack pMatrixStack, IRenderTypeBuffer.Impl pBuffer, LightTexture pLightTexture, ActiveRenderInfo pActiveRenderInfo, float pPartialTicks, ClippingHelper pClippingHelper)
     {
         final PlayerEntity player = mc.player;
         World level;
         if (player == null ||(level = player.level) == null) return;
 
-        Vector3d camera = mc.gameRenderer.getMainCamera().getPosition();
+        Vector3d camera = pActiveRenderInfo.getPosition();
         double camX = camera.x;
         double camY = camera.y;
         double camZ = camera.z;
 
-        RenderSystem.popMatrix();
-        RenderSystem.pushMatrix();
-
         ServerStageAreaProvider.getServerStageAreas(level).ifPresent(
                 areas -> {
-                    if (GraphicsFanciness.FABULOUS == mc.options.graphicsMode)
                     areas.getStageAreas().forEach(
                             (area) -> {
                                 IVertexBuilder vertexBuilder1 = pBuffer.getBuffer(RenderType.lightning());
@@ -59,17 +56,17 @@ public class RenderHandler
                                 StageAreaRenderer.renderFloatingText(new StringTextComponent(area.getTitle()),
                                                                      area.getAreaAABB().getCenter(),
                                                                      pMatrixStack,
-                                                                     pBuffer, mc.gameRenderer.getMainCamera(), -1);
+                                                                     pBuffer, pActiveRenderInfo, -1);
 
                                 StageAreaRenderer.renderFloatingText(new StringTextComponent("Audience Spawn"),
                                                                      new Vector3d(area.getAudienceSpawn().getX() + 0.5, area.getAudienceSpawn().getY() + 1.5, area.getAudienceSpawn().getZ() + 0.5),
                                                                      pMatrixStack,
-                                                                     pBuffer, mc.gameRenderer.getMainCamera(), -1);
+                                                                     pBuffer, pActiveRenderInfo, -1);
 
                                 StageAreaRenderer.renderFloatingText(new StringTextComponent("Performer Spawn"),
                                                                      new Vector3d(area.getPerformerSpawn().getX() + 0.5, area.getPerformerSpawn().getY() + 1.5, area.getPerformerSpawn().getZ() + 0.5),
                                                                      pMatrixStack,
-                                                                     pBuffer, mc.gameRenderer.getMainCamera(), -1);
+                                                                     pBuffer, pActiveRenderInfo, -1);
                             });
                 });
     }
